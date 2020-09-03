98e2707133edcea91b716b94173cf07643e10fdd
PaulDepew <paul@lostamongpines.com>
2020-09-02 22:04:03 -0700

    title done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import GraphicEqIcon from '@material-ui/icons/GraphicEq';
-import AccountCircle from '@material-ui/icons/AccountCircle';
+import Typical from 'react-typing-animation'
 import myTheme from '../theme';
-import {ReactComponent as DBLogoDark} from '../assets/DB_dark.svg'
+import {ReactComponent as DBLogoLite} from '../assets/DB_lite.svg'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -19,50 +18,49 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+    fontFamily:'bebas-neue-rounded',
+    marginLeft:'-15px',
+    fontSize:'58pt'
   },
 }));
 
 const Header = () => {
   const classes = useStyles();
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
 
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
-  };
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <div className={classes.root}>
       <AppBar style={myTheme.palette.companyNeutral} position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <DBLogoDark style={{width: '7vw'}} />
+            <DBLogoLite style={{width: '75px'}} />
           </IconButton>
-          <Typography variant="h2" style={{fontFamily:'bebas-neue-rounded', marginLeft:'-15px'}} className={classes.title}>
-            Doublebang Media
-          </Typography>
-          {auth && (
+          <Typical hideCursor={true} loop={true} className={classes.title} speed={50}>  
+          <span  style={{color:'#ffffff' }}>DoubleBang Design</span>
+          <Typical.Backspace delay={6000} count={18} />
+          <span style={{color:'#96454e' }}>UI/UX</span>
+          <Typical.Backspace delay={2000} count={5} />
+          <span style={{color:'#ffee9f' }}>Code</span>
+          <Typical.Backspace delay={2000} count={5} />
+          <span style={{color:'#7aa890' }}>Data </span>
+          <Typical.Backspace delay={2000} count={5} />
+          <span style={{color:'#7b6bb0' }}>Media</span>
+          <Typical.Backspace delay={2000} count={6} />
+          <Typical.Reset />
+          </Typical>
+          {/* <Typography variant="h2" style={{fontFamily:'bebas-neue-rounded', marginLeft:'-15px'}} className={classes.title}>
+            Media
+          </Typography> */}
             <div>
               <IconButton
                 aria-label="account of current user"
                 aria-controls="menu-appbar"
                 aria-haspopup="true"
-                onClick={handleMenu}
                 color="inherit"
               >
                 <GraphicEqIcon />
               </IconButton>
             </div>
-          )}
         </Toolbar>
       </AppBar>
     </div>
